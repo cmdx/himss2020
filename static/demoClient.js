17227fcd815fb57ea3fd004559bb49e17965188c
cmaddux <cmaddux@zoho.com>
2020-02-06 16:34:44 -0800

    Update static/demoClient.js

--- static/demoClient.js
+++ static/demoClient.js
@@ -532,7 +532,7 @@ var eventBus = new Vue();
   })
 
 // Socket.io set the path to the server
-  var socket = io('ws://172.30.33.29:8080', {forceNew: true});
+  var socket = io('ws://localhost:8080', {forceNew: true});
 
 //creation of room objects that will be replaced once rooms array has been broadcasted from server
   const rooms = [{
